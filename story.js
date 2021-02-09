2ba50d4ce9d7bffc66c2b6006fae8a6161415202
sabrinaorn <77697020+sabrinaorn@users.noreply.github.com>
2021-02-09 18:01:07 -0500

    Update story.js
    fixed brackets, comma, apostrophe and moved Deacon Brodie's to chapters section

--- story.js
+++ story.js
@@ -61,7 +61,8 @@ var config = {
             onChapterEnter: [],
             onChapterExit: []
 
-          }
+          },
+
             {
             id: 'other-identifier',
             title: 'Plant and Curio',
@@ -75,30 +76,29 @@ var config = {
             },
             onChapterEnter: [],
             onChapterExit: []
-
-          }
-          {
-          id: 'Temporary_Closures',
-          title: 'Bar Hopping on Elgin St Leads to Five Temporary Closures',
-          image: '.img/download.png',
-          description:'Elgin St. saw five temporary closures of bars and restaurants from two individuals who were awaiting their COVID test results. These individuals visted those locations while awaiting their tetst results and when the tests came back positive, the businesses voluntarily closed for two weeks to test their employees to ensure safe operations. It is unknown how much revenue was lost during these closures, however, it is important to note how badly that revenue was needed because of restricted operations. The affected businesses were:'
-          <ul>
-          <li>Deacon Brodie's</li>
-          <li>St Louis Bar and Grill</li>
-          <li>Elgin Beer Project</li>
-          <li>Lieutenants Pump</li>
-          <li>Waller Street Brewing</li>
-          </ul> 
-          location: {
-              center: [-75.68819, 45.41577],
-              zoom: 13.5,
-              pitch: 60,
-              bearing: -43.2
           },
-          onChapterEnter: [],
-          onChapterExit: []
-}
-        
+
+          {
+              id: 'Temporary_Closures',
+              title: 'Bar Hopping on Elgin St Leads to Five Temporary Closures',
+              image: '.img/download.png',
+              description:'Elgin St. saw five temporary closures of bars and restaurants from two individuals who were awaiting their COVID test results. These individuals visted those locations while awaiting their tetst results and when the tests came back positive, the businesses voluntarily closed for two weeks to test their employees to ensure safe operations. It is unknown how much revenue was lost during these closures, however, it is important to note how badly that revenue was needed because of restricted operations. The affected businesses were:',
+              <ul>
+              <li> Deacon Brodies </li>
+              <li> St Louis Bar and Grill</li>
+              <li> Elgin Beer Project</li>
+              <li> Lieutenants Pump</li>
+              <li> Waller Street Brewing</li>
+              </ul>
+              location: {
+                  center: [-75.68819, 45.41577],
+                  zoom: 13.5,
+                  pitch: 60,
+                  bearing: -43.2
+              },
+              onChapterEnter: [],
+              onChapterExit: []
+            }
+
         ]
         };
-
