c0b15e64560afb1415035cc4d0d1b69aadc74b09
therev14 <78615324+therev14@users.noreply.github.com>
2021-02-10 20:45:35 -0500

    Update story.js

--- story.js
+++ story.js
@@ -84,7 +84,7 @@ var config = {
               id: 'Temporary_Closures',
               title: 'Bar Hopping on Elgin St Leads to Five Temporary Closures',
               image: '.img/download.png',
-              description:'Elgin St. saw five temporary closures of bars and restaurants from two individuals who were awaiting their COVID test results. These individuals visted those locations while awaiting their tetst results and when the tests came back positive, the businesses voluntarily closed for two weeks to test their employees to ensure safe operations. It is unknown how much revenue was lost during these closures, however, it is important to note how badly that revenue was needed because of restricted operations. The affected businesses were:',
+              description:'Elgin St. saw five temporary closures of bars and restaurants from two individuals who were awaiting their COVID test results. These individuals visted those locations while awaiting their tetst results and when the tests came back positive, the businesses voluntarily closed for two weeks to test their employees to ensure safe operations. It is unknown how much revenue was lost during these closures, however, it is important to note how badly that revenue was needed because of restricted operations. The affected businesses were:
 
             <ul>
               <li> Deacon Brodies </li>
@@ -92,7 +92,7 @@ var config = {
               <li> Elgin Beer Project</li>
               <li> Lieutenants Pump</li>
               <li> Waller Street Brewing</li>
-              </ul>
+              </ul>',
               location: {
                   center: [-75.68819, 45.41577],
                   zoom: 13.5,
